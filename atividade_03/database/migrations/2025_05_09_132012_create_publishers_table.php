4cd6ea16279aa58aca0cd6c6f77b7df6ec825842
PdrVinicius <pedro.lopesxavier@gmail.com>
2025-05-16 22:08:05 -0300

    corrigindo migrations

--- atividade_03/database/migrations/2025_05_09_132012_create_publishers_table.php
+++ atividade_03/database/migrations/2025_05_09_132012_create_publishers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('publishers', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('addres')->nullable();
+            $table->string('address')->nullable();
             $table->timestamps();
         });
     }
