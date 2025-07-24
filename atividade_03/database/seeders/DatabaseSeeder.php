8db4426c4941d01c31f5f754f4eee612e2a6418c
PdrVinicius <pedro.lopesxavier@gmail.com>
2025-07-24 20:18:10 -0300

    corrigindo atv 3

--- atividade_03/database/seeders/DatabaseSeeder.php
+++ atividade_03/database/seeders/DatabaseSeeder.php
@@ -3,17 +3,15 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as FakerFactory;
+
 class DatabaseSeeder extends Seeder
 {
-    
     public function run()
     {
-        FakerFactory::create()->unique(true);
         $this->call([
             CategorySeeder::class,
             AuthorPublisherBookSeeder::class,
         ]);
     }
-}}
 }
+
