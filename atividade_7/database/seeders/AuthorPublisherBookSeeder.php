87054c9d69e6e56ef40439c2a382995c32997f6b
PdrVinicius <pedro.lopesxavier@gmail.com>
2025-07-24 21:25:32 -0300

    adicionar campo de imagem de capa na tabela books

---
+++ atividade_7/database/seeders/AuthorPublisherBookSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Publisher;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AuthorPublisherBookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Author::factory(100)->create()->each(function($author) {
+            $publisher = Publisher::factory()->create();
+            $author->books()->createMany(
+                Book::factory(10)->make([
+                    'category_id' => Category::inRandomOrder()->first()->id,
+                    'publisher_id' => $publisher->id,
+                ])->toArray()
+            );
+        });
+    }
+}
